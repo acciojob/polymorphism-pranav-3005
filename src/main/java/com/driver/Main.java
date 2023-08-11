8088aaa9e0f75fa6d3cbbcda10fa2a36b852d5a9
pranav <pranavanandth19@gamil.com>
2023-08-11 08:41:50 +0530

    1st attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
 public class Main {
+    static class Product
+    {
+        public int product(int x,int y)
+        {
+            return 0;
+        }
 
+        public int product(int x,int y,int z)
+        {
+            return 0;
+        }
+
+        public double product(double x,double y)
+        {
+            return 0;
+        }
+
+    }
+
+    public static void main(String args[])
+    {
+        Product p=new Product();
+
+        p.product(0,0);
+        p.product(0,0,0);
+        p.product(0.0,0.0);
+    }
 }
